641e176ebe84530304700335dadfbc5688c7a042
amanape <stephanpsaras@gmail.com>
2022-05-05 17:30:39 +0200

    Connect context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { TodoProvider } from './context/TodoContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.StrictMode>,
 );
