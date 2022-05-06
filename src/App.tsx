ec8b400e4f0aa0a248330a1f50e1ae2bba85a86a
amanape <stephanpsaras@gmail.com>
2022-05-06 20:03:50 +0200

    Remove dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
 import React from 'react';
-import { nanoid } from 'nanoid';
 import TaskList from './components/TaskList/TaskList';
 import TaskInputBar from './components/TaskInputBar/TaskInputBar';
 
 import './sass/main.scss';
-import { ITask } from './shared/types';
-
-const DUMMY_TASKS: ITask[] = [
-  {
-    id: nanoid(),
-    title: 'Task 1',
-  },
-  {
-    id: nanoid(),
-    title: 'Task 2',
-  },
-  {
-    id: nanoid(),
-    title: 'Task 3',
-  },
-];
 
 const App = () => (
   <div className="App">
     <TaskInputBar />
     <div className="task-container">
       <strong>Tasks</strong>
-      <TaskList tasks={DUMMY_TASKS} />
+      <TaskList />
     </div>
     <div>
       <label htmlFor="filter-complete">
