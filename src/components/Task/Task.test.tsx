265c24e286a5ccb2de770dc92a03008e76ef867b
amanape <stephanpsaras@gmail.com>
2022-05-10 18:07:31 +0200

    Add tests and component logic

--- src/components/Task/Task.test.tsx
+++ src/components/Task/Task.test.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Task from './Task';
+import { ITask } from '../../shared/types';
 
-it('should output the correct task data', () => {
-  render(<Task title="Complete challenge." id="1" />);
-  expect(screen.getByText('Complete challenge.')).toBeInTheDocument();
+describe('Task', () => {
+  const removeTask = jest.fn();
+  const updateTask = jest.fn();
+  const stubTodoContextHook = () => ({
+    removeTask,
+    updateTask,
+  });
+  const dummyTask: ITask = {
+    id: '1',
+    title: 'test',
+  };
+  it('should output the correct task data', () => {
+    render(<Task task={dummyTask} />);
+
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+
+  describe('when the "Delete" button is clicked', () => {
+    it('should call removeTask', () => {
+      render(<Task task={dummyTask} useTodoContextHook={stubTodoContextHook} />);
+      const delButton = screen.getByRole('button', { name: 'Delete' });
+
+      delButton.click();
+
+      expect(removeTask).toHaveBeenCalledWith(dummyTask.id);
+    });
+  });
+
+  describe('when the "Edit" button is clicked', () => {
+    it('should display the edit modal', () => {
+      render(<Task task={dummyTask} useTodoContextHook={stubTodoContextHook} />);
+      const editButton = screen.getByRole('button', { name: 'Edit' });
+
+      editButton.click();
+    });
+  });
 });
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ITask } from '../../shared/types';
+import { useTodoContext } from '../../context/TodoContext';
+import EditModal from './EditModal/EditModal';
 
-const Task: React.FC<ITask> = ({ title, id }) => (
-  <li className="task">
-    <label htmlFor={`task-${id}`}>
-      <input type="checkbox" id={`task-${id}`} />
-      {title}
-    </label>
-    <div className="btn-container">
-      <button type="button" className="btn-edit">Edit</button>
-      <button type="button" className="btn-delete">Delete</button>
-    </div>
-  </li>
-);
+interface TaskProps {
+  task: ITask;
+  useTodoContextHook?: () => Pick<ReturnType<typeof useTodoContext>, 'removeTask'>
+}
+
+const Task: React.FC<TaskProps> = ({ task, useTodoContextHook = useTodoContext }) => {
+  const [show, setShow] = useState(false);
+
+  const { removeTask } = useTodoContextHook();
+  const { id, title } = task;
+
+  return (
+    <li className="task">
+      <label htmlFor={`task-${id}`}>
+        <input type="checkbox" id={`task-${id}`} />
+        {title}
+      </label>
+      <div className="btn-container">
+        <button type="button" className="btn-edit" onClick={() => setShow(true)}>Edit</button>
+        <button type="button" className="btn-delete" onClick={() => removeTask(id)}>Delete</button>
+      </div>
+      {show && <EditModal taskId={id} modalCloseHandler={() => setShow(false)} />}
+    </li>
+  );
+};
+
+Task.defaultProps = {
+  useTodoContextHook: useTodoContext,
+};
 
 export default Task;
